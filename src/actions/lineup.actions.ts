822efdaeb02c4812ed08ec933db8cde86c513dab
Ernesto Rendon <egrendon.dev@gmail.com>
2016-10-16 20:43:51 -0700

    added inital configs for linting

--- src/actions/lineup.actions.ts
+++ src/actions/lineup.actions.ts
@@ -1,17 +1,16 @@
-import { PARTY_JOINED, PARTY_LEFT } from '../constants';
-import { Injectable } from '@angular/core';
-import { NgRedux } from 'ng2-redux';
-import { IAppState } from '../store';
-import { PartyService } from '../services/party';
+import {PARTY_JOINED, PARTY_LEFT} from '../constants';
+import {Injectable} from '@angular/core';
+import {NgRedux} from 'ng2-redux';
+import {IAppState} from '../store';
+import {PartyService} from '../services/party';
 
 @Injectable()
 export class LineupActions {
-  constructor(
-    private _ngRedux: NgRedux<IAppState>,
-    private _party: PartyService) {}
+  constructor(private _ngRedux: NgRedux<IAppState>, private _partyService: PartyService) {
+  }
 
-  joinLine = ({ numberOfPeople, partyName }) => {
-    return this._party.getNextPartyId().then(partyId => {
+  joinLine = ({numberOfPeople, partyName}) => {
+    return this._partyService.getNextPartyId().then(partyId => {
       return this._ngRedux.dispatch({
         type: PARTY_JOINED,
         payload: {
@@ -23,7 +22,7 @@ export class LineupActions {
     });
   };
 
-  leaveLine = ({ partyId }) => {
+  leaveLine = ({partyId}) => {
     return this._ngRedux.dispatch({
       type: PARTY_LEFT,
       payload: {
